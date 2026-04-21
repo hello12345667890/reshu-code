6b3afe861e6eba73a9c2e9c0f30d41cf97dbfb1e
hello12345667890 <sivabalakrishna.d@gmail.com>
2026-04-21 08:38:06 +0530

    Add files via upload

---
+++ pofit.py
@@ -0,0 +1,7 @@
+actual_cost=float(input("Please enter the actual price: "))
+sale_amount=float(input("Please enter the sales amount: "))
+if(sale_amount>actual_cost):
+    amount= sale_amount-actual_cost
+    print("Total profit= {0.}".format(amount))
+else:
+    print("NO PROFIT")
---
+++ positive.py
@@ -0,0 +1,7 @@
+number=int(input("please enter your number "))
+if number>0:
+    print("your number is positive")
+elif number<0:
+    print("your number is negitive.")
+else:
+    print("your number is zero.")
